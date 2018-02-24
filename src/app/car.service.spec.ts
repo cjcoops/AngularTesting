f6b2474b09b4cbcef8d567ee9115d4e0a74c295d
Chris Cooper <cjcooper252@gmail.com>
2018-02-24 14:33:45 +0000

    mocks return values of engine class

--- src/app/car.service.spec.ts
+++ src/app/car.service.spec.ts
@@ -2,21 +2,46 @@ import { TestBed, inject } from "@angular/core/testing";
 
 import { Car } from "./car.service";
 import { Engine } from "./engine.service";
+import { Injectable } from "@angular/core";
+
+// fake class
+@Injectable()
+class V8Engine {
+  getHorsepower() {
+    return 400;
+  }
+
+  getName() {
+    return "V8 Engine";
+  }
+}
 
 describe("Car", () => {
   let subject: Car;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [Car, Engine]
+      providers: [
+        Car,
+        {
+          provide: Engine,
+          useClass: V8Engine
+        }
+      ]
     });
+
+    // Mocking Engine return values
+    spyOn(Engine.prototype, "getHorsepower").and.returnValue(400);
+    spyOn(Engine.prototype, "getName").and.returnValue("V8 Engine");
   });
 
-  beforeEach(inject([Car], (car: Car) => {
-    subject = car;
-  }));
+  beforeEach(
+    inject([Car], (car: Car) => {
+      subject = car;
+    })
+  );
 
   it("should display name with engine", () => {
-    expect(subject.getName()).toEqual("Car with Basic engine(150 HP)");
+    expect(subject.getName()).toEqual("Car with V8 Engine(400 HP)");
   });
 });
